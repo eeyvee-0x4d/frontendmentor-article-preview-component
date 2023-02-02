4090bc4a68a8c350f06b12afd09964b1f6a87156
Al Vincent Musa <alvincentvibar.musa@bicol-u.edu.ph>
2023-02-02 15:59:23 +0800

    done

---
+++ index.js
@@ -0,0 +1,6 @@
+let whereTheMagicHappens = document.querySelector(".where-the-magic-happens")
+let shareLinks = document.querySelector(".share-links")
+
+const toggleShareLinks = () => {
+    whereTheMagicHappens.classList.toggle("active")
+}
